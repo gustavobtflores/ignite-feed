59ffdc876cb8781ac656d9866b1dc3a46eb6560a
Gustavo Bernardo <gustavobtflores@gmail.com>
2022-06-13 10:37:08 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import styles from "./style.module.scss";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/ignite-logo.svg";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <h1>
         <img src={logo} alt="Ignite logo" />
-        <span>Ignite feed</span>
+        <span>Ignite Feed</span>
       </h1>
     </header>
   );
