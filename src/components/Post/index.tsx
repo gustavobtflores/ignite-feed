77e1dea2427e9c98ce6955f9ed461a43984c0d6f
Gustavo Bernardo <gustavobtflores@gmail.com>
2022-06-17 22:19:48 -0300

    finishes post component

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -15,8 +15,26 @@ export const Post: React.FC = () => {
             <span>Front-end developer</span>
           </div>
         </div>
-        <span className={styles["posted-time"]}>Publicado há 13min</span>
+        <time title="8 de maio as 20:34h" dateTime="2022-05-11 20:34:20" className={styles["posted-time"]}>
+          Publicado há 1h
+        </time>
       </header>
+
+      <div className={styles.content}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quo.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum!</p>
+        <a href="#">jane.design/doctorcare</a>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe seu comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 };
